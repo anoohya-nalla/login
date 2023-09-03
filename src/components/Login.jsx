b7a116644cd1029d0580f59551423d8184068433
Anoohya-Nalla <nallaanoohya@gmail.com>
2023-09-03 16:29:55 +0530

    feat: add loadingButton in login screen

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,18 +2,20 @@ import React, { useState } from "react";
 import axios from "axios";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
 import { toast } from "react-toastify";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import backgroundImage from "../assets/background.jpg";
-import { Checkbox, FormControlLabel, Link } from "@mui/material";
+import { Checkbox, FormControlLabel, Link, Typography } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
 
 const Login = ({ onLogin }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleLogin = async () => {
+    setLoading(true);
     try {
       const response = await axios.post(
         "https://api.escuelajs.co/api/v1/auth/login",
@@ -36,6 +38,7 @@ const Login = ({ onLogin }) => {
         position: "bottom-right",
       });
     }
+    setLoading(false);
   };
 
   const isMobileScreen = useMediaQuery("(max-width: 900px)");
@@ -52,6 +55,7 @@ const Login = ({ onLogin }) => {
           item
           md={6}
           style={{
+            position: "relative",
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
@@ -67,6 +71,25 @@ const Login = ({ onLogin }) => {
               objectFit: "cover",
             }}
           />
+          <div
+            style={{
+              position: "absolute",
+              bottom: "20px",
+              left: "20px",
+              color: "#fff",
+              textAlign: "left",
+              padding: "40px",
+            }}
+          >
+            <Typography variant="h5" style={{ fontWeight: "bold" }}>
+              Mindia Admin Portal
+            </Typography>
+            <Typography variant="body2" style={{ fontSize: "12px" }}>
+              From user management to content updates, analytics to
+              monetization, our intuitive tools empower you to seamlessly
+              oversee and optimize every aspect of your gaming ecosystem
+            </Typography>
+          </div>
         </Grid>
       )}
 
@@ -83,6 +106,12 @@ const Login = ({ onLogin }) => {
         }}
       >
         <h2>Login</h2>
+        <Typography
+          variant="body2"
+          style={{ fontSize: "14px", color: "#1976d2", textAlign: "left" }}
+        >
+          Access your math project account using the credentials emailed to you
+        </Typography>
         <TextField
           label="Email"
           type="text"
@@ -110,27 +139,38 @@ const Login = ({ onLogin }) => {
           }}
         >
           <FormControlLabel
+            style={{ color: "#1976d2" }}
             control={
               <Checkbox
                 checked={rememberMe}
                 onChange={() => setRememberMe(!rememberMe)}
               />
             }
-            label="Remember Me"
+            label={
+              <Typography variant="body1" style={{ fontSize: "15px" }}>
+                Remember me
+              </Typography>
+            }
           />
-          <Link href="/forgot-password" variant="body2">
-            Forgot Password?
+          <Link
+            href="/forgot-password"
+            variant="body2"
+            style={{ textDecoration: "none" }}
+          >
+            Forgot password?
           </Link>
         </div>
 
-        <Button
+        <LoadingButton
+          loading={loading}
+          loadingPosition="start"
           variant="contained"
           color="primary"
           onClick={handleLogin}
           fullWidth
         >
           Login
-        </Button>
+        </LoadingButton>
       </Grid>
     </Grid>
   );
